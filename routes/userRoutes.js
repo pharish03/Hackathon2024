05e8161efe2bea6824d6fb38985bac83e99f9c71
James Reneo <jayreneo123@gmail.com>
2024-04-06 20:39:16 -0400

    adding depost amount front end and function

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,24 +1,26 @@
-const express = require('express');
-const controller = require('../controllers/userController');
+const express = require("express");
+const controller = require("../controllers/userController");
 
 const router = express.Router();
 
 // send html form for creating a new user account
-router.get('/signup', controller.new);
+router.get("/signup", controller.new);
 
 // create a new user account
-router.post('/', controller.create);
+router.post("/", controller.create);
 
 // send html for logging in
-router.get('/login', controller.getUserLogin);
+router.get("/login", controller.getUserLogin);
 
 // authenticate user's login
-router.post('/login', controller.login);
+router.post("/login", controller.login);
 
 // send user's profile page
-router.get('/overview', controller.profile);
+router.get("/overview", controller.profile);
 
 // logout a user
-router.get('/logout', controller.logout);
+router.get("/logout", controller.logout);
+
+router.post("/overview", controller.updateBalance);
 
 module.exports = router;
